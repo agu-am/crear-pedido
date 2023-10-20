fc43310a317445acc9a5cc01a6dbfafa48f9d249
agu-am <agustin.cs96@gmail.com>
2023-10-20 18:50:56 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true,
+    // host: true,
   },
   plugins: [react()],
 })
