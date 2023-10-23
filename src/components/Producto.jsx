8060d950066caeec3b6f724a8c17be91aeca99b1
agu-am <agustin.cs96@gmail.com>
2023-10-23 16:24:48 -0300

    [add] cantidades de pedidos en componente producto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,29 +1,54 @@
+import { useEffect, useState } from "react"
 import usePedido from "../hooks/usePedido"
 
 const Producto = ({ producto }) => {
     const { handleSetPedido, handleDecrementPedido, pedido } = usePedido()
     const { nombre, sku, cantidad } = producto
+    const [cantidadActual, setCantidadActual] = useState()
+
+    const obtenerCantidadActual = () => {
+        const productoExistente = pedido.productos.find(p => p.sku === producto.sku);
+        if (productoExistente) {
+            setCantidadActual(productoExistente.cantidad);
+        } else {
+            // Si el producto no existe en el carrito, establece la cantidad actual en 0
+            setCantidadActual(0);
+        }
+    }
+
+    useEffect(() => {
+        obtenerCantidadActual()
+    }, [pedido])
+
+
     return (
         <div className="flex p-3 justify-between items-center aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:h-28">
             <div>
                 <h3 className="text-sm text-gray-700">{nombre}</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">SKU: {sku}</p>
             </div>
-            <div className="flex flex-col gap-2">
-                <button
-                    type="button"
-                    className="h-8 w-8 justify-center text-white bg-green-700 hover:bg-green-800 rounded-md text-base text-center items-center font-bold"
-                    onClick={() => handleSetPedido({ nombre, sku, cantidad })}
-                >
-                    + 
-                </button>
-                <button
-                    type="button"
-                    className="h-8 w-8 justify-center text-white bg-green-700 hover:bg-green-800 rounded-md text-base text-center items-center font-bold"
-                    onClick={() => handleDecrementPedido({ nombre, sku, cantidad })}
-                >
-                    -
-                </button>
+            <div className="flex flex-col gap-2 items-end">
+                <div className=" flex flex-col w-3/12 gap-1 justify-center items-end">
+                    <button
+                        type="button"
+                        className="h-8 w-8 justify-center self-center text-white bg-green-700 hover:bg-green-800 rounded-md text-base text-center items-center font-bold"
+                        onClick={() => handleSetPedido({ nombre, sku, cantidad })}
+                    >
+                        +
+                    </button>
+                    <input
+                        className="w-10/12 self-center text-center"
+                        type="number"
+                        defaultValue={cantidadActual}
+                    />
+                    <button
+                        type="button"
+                        className="h-8 w-8 justify-center self-center text-white bg-green-700 hover:bg-green-800 rounded-md text-base text-center items-center font-bold"
+                        onClick={() => handleDecrementPedido({ nombre, sku, cantidad })}
+                    >
+                        -
+                    </button>
+                </div>
             </div>
         </div>
     )
