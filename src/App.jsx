518cc3802a967722eef9102c1e3456c064546704
agu-am <agustin.cs96@gmail.com>
2023-11-01 13:06:34 -0300

    [add] NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import Ordenes from "./pages/Ordenes"
 import Login from "./pages/Login"
 import NotFound from "./pages/NotFound"
+import NavBar from "./components/NavBar"
 
 function App() {
 
@@ -12,12 +13,13 @@ function App() {
 
   return (
     <PedidosProvider>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login setToken={setToken} />}/>
-          <Route path="/ordenes" element={token ? <Ordenes /> : <Navigate to="/login"/>}/>
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/ordenes" element={token ? <Ordenes /> : <Navigate to="/login" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </PedidosProvider>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaUserAlt } from 'react-icons/fa'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='p-2 flex justify-between items-center border-b-2'>
+      <Link to='/'>
+        <img
+          className='w-14'
+          src="./public/assets/img/logoPedidosPaul.png"
+          alt="Logo de Pedidos Paul"
+        />
+      </Link>
+      <div>
+        <Link to='/ordenes'>
+          <FaUserAlt size="2rem"/>
+        </Link>
+      </div>
+    </div>
   )
 }
 
