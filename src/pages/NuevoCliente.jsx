cf05a3c815765f69f291e21be9e01fba18c508e8
agu-am <agustin.cs96@gmail.com>
2024-02-09 13:18:45 -0300

    [add] estilos a NuevoCliente

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -2,45 +2,72 @@ import axios from "axios";
 import { useState } from "react";
 
 const NuevoCliente = () => {
-  
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const predefinedPassword = 'Bplmcfc10';
-  const token = localStorage.getItem('token');
 
-  const handleSubmit = async (event) => {
-      event.preventDefault();
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const predefinedPassword = 'Bplmcfc10';
+    const token = localStorage.getItem('token');
 
-      const data = {
-          username: username,
-          password: predefinedPassword,
-          email: email,
-          roles: ['customer'] // Asegúrate de que el rol "customer" existe en tu instalación de WordPress
-      };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-      const config = {
-          headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-          }
-      };
+        const data = {
+            username: username,
+            password: predefinedPassword,
+            email: email,
+            roles: ['customer'] // Asegúrate de que el rol "customer" existe en tu instalación de WordPress
+        };
 
-      try {
-          const response = await axios.post('https://pedidosprueba.agustinjs.com/wp-json/wp/v2/users', data, config);
-          console.log('User added successfully', response.data);
-      } catch (error) {
-          console.error('Error adding user', error);
-          console.error(error.response.data.message);
-      }
-  }
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        };
 
-  return (
-      <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
-          <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
-          <button type="submit">Add User</button>
-      </form>
-  );
+        try {
+            const response = await axios.post('https://pedidosprueba.agustinjs.com/wp-json/wp/v2/users', data, config);
+            console.log('User added successfully', response.data);
+        } catch (error) {
+            console.error('Error adding user', error);
+            console.error(error.response.data.message);
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center w-full h-screen">
+            <form
+                className="flex flex-col gap-2 border rounded shadow-sm items-center justify-center xl:w-4/12 h-2/4"
+                onSubmit={handleSubmit}
+            >
+                <div className="mb-2 self-center bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-white rounded pb-1 xl:w-6/12">
+                    <input
+                        className="flex w-full bg-white text-xl text-black rounded border-white pt-2"
+                        type="text"
+                        placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className="mb-2 self-center bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-white rounded pb-1 xl:w-6/12">
+                    <input
+                        className="flex w-full bg-white text-xl text-black rounded border-white pt-2"
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <button
+                    className='w-6/12 rounded-md font-bold text-white uppercase p-2 bg-gradient-to-r from-green-400 via-green-500 to-green-600 xl:w-6/12'
+                    type="submit"
+                >
+                    Agregar Cliente
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default NuevoCliente
